67d457bc31561046ce3289b59124a6e0d37e938f
Flavio Hernandes <hernandes.flavio@gmail.com>
2020-05-26 19:54:56 -0300

    First description

--- src/Item/ItemSetTrait.php
+++ src/Item/ItemSetTrait.php
@@ -1,1 +1,21 @@
 <?php
+namespace MuOnline\Item;
+
+trait ItemSetTrait
+{
+
+    private $item;
+
+    public function setItem(Item $item): self
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    public function getItem(): Item
+    {
+        return $this->item;
+    }
+
+}
