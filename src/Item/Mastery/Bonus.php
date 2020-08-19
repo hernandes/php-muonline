266ededaca8f06161c40098ead8b32903a0dc149
Flavio Hernandes <hernandes.flavio@gmail.com>
2020-08-19 16:46:18 -0300

    MasteryBonus pojo added

--- src/Item/Mastery/Bonus.php
+++ src/Item/Mastery/Bonus.php
@@ -4,6 +4,33 @@ namespace MuOnline\Item\Mastery;
 class Bonus
 {
 
+    /**
+     * @var int|null
+     */
+    private $value;
 
+    public function __construct(?int $value = null)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function set(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function get(): ?int
+    {
+        return $this->value;
+    }
 
 }
