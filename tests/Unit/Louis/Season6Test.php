713e2497d9dbeb0495cea124b30b0ba59f3c603d
Flavio Hernandes <hernandes.flavio@gmail.com>
2021-07-09 16:43:09 -0300

    Added more hex to test

--- tests/Unit/Louis/Season6Test.php
+++ tests/Unit/Louis/Season6Test.php
@@ -3,20 +3,23 @@
 use MuOnline\Team\Team;
 use MuOnline\Item\Item;
 
-test('louis season 6 test parse and make hex', function () {
+it('louis season 6 test parse and make hex', function () {
 
     $team = Team::factory('louis', 6);
     Team::set($team);
 
     $hexes = [
-        '786FFF00275EF5FF00FF'
+        '004A14000000AA00000000FFFFFFFFFF',
+        '12d567000000787f000000ffffffffff',
+        '1f8d6d000000613f000000bd04ffffff',
+        '05FF31000000757F00406DFFFFFFFFFF'
     ];
 
     foreach ($hexes as $hex) {
         $item = new Item();
         $item->parse($hex);
 
-        $this->assertEquals($hex, $item->make());
+        expect($hex)->toBe($item->make());
     }
 
 });
